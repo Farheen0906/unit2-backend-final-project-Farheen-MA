7757d5544bce37773f2bf5e02dd2a20bf364ea11
Farheen <farhashaik99@gmail.com>
2026-09-02 22:03:53 -0400

    Added ContactRequest repository

---
+++ src/main/java/com/homecooked/backend/repositories/ContactRequestRepository.java
@@ -0,0 +1,10 @@
+package com.homecooked.backend.repositories;
+
+import com.homecooked.backend.model.ContactRequest;
+import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRequestRepository extends JpaRepository<ContactRequest, Integer> {
+}
