c14eb1d542f076d625000765df5ae1b1139c4a7f
Farheen <farhashaik99@gmail.com>
2026-09-02 21:24:42 -0400

    Added user  repository

---
+++ src/main/java/com/homecooked/backend/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.homecooked.backend.repository;
+
+import com.homecooked.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
