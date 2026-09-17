fe13e1890b7d3af0798275b1222a249206b49e86
Farheen <farhashaik99@gmail.com>
2026-09-16 20:56:55 -0400

    Added code for filtering by category

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -5,6 +5,8 @@ import './Menu.css';
 function Menu({ addToCart }) {
   //props : addToCart function from App.jsx
     const [menuItems, setMenuItems] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
     useEffect(() => {
         async function getMeals() {
             try {
@@ -27,16 +29,52 @@ function Menu({ addToCart }) {
         getMeals();
     }, []);
 
-  return (
+    // User can filter the meals based on the category selected
+    // Building a list of category names, with no duplicates - Starting with "All" already in the list
+    const categories = ['All'];
+
+    for (let i = 0; i < menuItems.length; i++) {
+        const currentCategory = menuItems[i].category;
+
+        // Only adds it if it's not already in our categories list
+        if (categories.includes(currentCategory) === false) {
+            categories.push(currentCategory);
+        }
+    }
+    // Deciding which items to actually display, based on the selected category
+    let filteredItems = [];
+
+    if (selectedCategory === 'All') {
+        filteredItems = menuItems;
+    } else {
+        for (let i = 0; i < menuItems.length; i++) {
+            if (menuItems[i].category === selectedCategory) {
+                filteredItems.push(menuItems[i]);
+            }
+        }
+    }
+    return (
     <div className='menu-page'>
       {/* Page Header Section */}
       <div className='menu-page-header'>
         <h1 className='menu-page-title'>Our Menu</h1>
         <p className='menu-page-content'>Select an item to get started!!!!</p>
       </div>
+        {/* Filter by category section */}
+        <div className='menu-filter-bar'>
+            {categories.map((category) => (
+                <button
+                    key={category}
+                    className={selectedCategory === category ? 'filter-btn active' : 'filter-btn'}
+                    onClick={() => setSelectedCategory(category)}
+                >
+                    {category}
+                </button>
+            ))}
+        </div>
       {/* Menu Grid */}
       <div className='menu-grid'>
-        {menuItems.map((item) => (
+        {filteredItems.map((item)=> (
           <MenuCard key={item.id}
             item={item}
             addToCart={addToCart}
