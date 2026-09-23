f24744667c920e2898c37c5a5e3e852539bd841f
Farheen <farhashaik99@gmail.com>
2026-09-23 18:58:50 -0400

    Added code to pull meal details and images on order confirmation page

--- backend unit 2 java/src/main/java/com/homecooked/backend/controllers/OrderItemController.java
+++ backend unit 2 java/src/main/java/com/homecooked/backend/controllers/OrderItemController.java
@@ -5,6 +5,8 @@ import com.homecooked.backend.repositories.OrderItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/order-items")
 @CrossOrigin(origins = "http://localhost:5173")
